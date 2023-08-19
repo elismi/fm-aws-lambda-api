b9533dc5e203336c41db033388309e6d9fa59a52
elismi <sile.h@live.com>
2023-08-20 01:37:36 +0800

    Dual Function Mapping

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,10 +1,16 @@
-from flask import Flask, jsonify
+from flask import (Flask, jsonify)
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/hello')
 def hello():
-    return jsonify(message='Hello from Flask on AWS Lambda!')
+    return jsonify(status=200, message='Hello Flask!')
+
+
+@app.route('/yolo')
+def yolo():
+    return jsonify(status=200, message='Yolo Flask!')
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- hello_world/lambda_function.py
+++ hello_world/lambda_function.py
@@ -1,7 +1,6 @@
+from app import app
+import awsgi
+
 
 def lambda_handler(event, context):
-    return {
-        'statusCode': 200,
-        'body': "success!"
-    }
     return awsgi.response(app, event, context)
